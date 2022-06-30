143bb895aa4f2734b526f974735d172f2acff4e3
CosmoAndrade <cosmo.andrade@hotmail.com>
2022-06-29 21:40:24 -0300

    Projeto finApi parte 05 atualizando para o português

--- src/index.js
+++ src/index.js
@@ -7,28 +7,29 @@ app.use(express.json());
 
 const port = 3000
 
+
 //criando um array para armazenar nossas informações em tempo de execução
-const customers = [];
+const clientes = [];
 
 //middleware de verificação de contas.
-function verifyIfExistsAccountCPF (request, response, next) {
+function verificaContaExistenteCPF (request, response, next) {
     const { cpf } = request.headers;
-    const customer = customers.find((customer) => customer.cpf == cpf);
-    if(!customer){
-        return response.status(400).json({error: "Customer not found!"});
+    const cliente = clientes.find((cliente) => cliente.cpf == cpf);
+    if(!cliente){
+        return response.status(400).json({error: "cliente not found!"});
     }
 
-    request.customer = customer;
+    request.cliente = cliente;
     return next();
 }
 
 //calculando o balanço da conta.
-function getBalance(statement){
-    const balance = statement.reduce((acc, operation) => {
+function getSaldo(extrato){
+    const saldo = extrato.reduce((acc, operation) => {
         if(operation.type === 'credit') {
-            return acc + operation.amount;      
+            return acc + operation.quantidade;      
         }else {
-            return acc - operation.amount;
+            return acc - operation.quantidade;
         }
     }, 0)
 }
@@ -38,74 +39,74 @@ function getBalance(statement){
  * cpf - string
  * nome - string
  * id - uuid
- * statement - []
+ * extrato - [] extrato
  */
 
-//como sabemos o post é o método para criar, usaremos /account como o recurso
-app.post("/account", (request, response) => {
+//como sabemos o post é o método para criar, usaremos /conta como o recurso
+app.post("/conta", (request, response) => {
     const { cpf, name } = request.body; //pegando cpf e nome utilizando conceito de desestruturação
     
     //Não deve ser possível cadastrar uma conta de CPF já existente.
-    const customerAlreadyExists = customers.some(
-        (customer) => customer.cpf === cpf
+    const clienteJaExiste = clientes.some(
+        (cliente) => cliente.cpf === cpf
     );
     //regra caso o cpf já esteja cadastrado
-    if (customerAlreadyExists){
-        return response.status(400).json({error: "Customer already exists!"});
+    if (clienteJaExiste){
+        return response.status(400).json({error: "cliente Já existe!"});
     }    
         
-    customers.push({
+    clientes.push({
         cpf,
         name,
         id: uuidv4(),
-        statement: []
+        extrato: []
     });
     
     return response.status(201).send();
 });
 
 
-//usamos o método get para buscar, usaremos /statement como recurso
-app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
-    const { customer } = request
-    return response.json(customer.statement);
+//usamos o método get para buscar, usaremos /extrato como recurso
+app.get("/extrato/", verificaContaExistenteCPF, (request, response) => {
+    const { cliente } = request
+    return response.json(cliente.extrato);
 });
 
 //fazendo depósito na conta
-app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
-    const { description, amount } = request.body;
-    const { customer } = request;
+app.post("/deposito", verificaContaExistenteCPF, (request, response) => {
+    const { description, quantidade } = request.body;
+    const { cliente } = request;
 
-    const statementOperation = {
+    const extratoOperation = {
         description,
-        amount,
+        quantidade,
         created_at: new Date(),
         type: "credit"
     };
 
-    customer.statement.push(statementOperation);
+    cliente.extrato.push(extratoOperation);
     return response.status(201).send();
 });
 
 
 //realizando saque na conta
-app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
-    const { amount } = request.body;
-    const { customer } = request;
+app.post("/retirar", verificaContaExistenteCPF, (request, response) => {
+    const { quantidade } = request.body;
+    const { cliente } = request;
 
-    const balance = getBalance(customer.statement);
+    const saldo = getSaldo(cliente.extrato);
 
-    if(balance < amount) {
-        return response.status(400).json({error: "Insufficient funds!"})
+    if(saldo < quantidade) {
+        return response.status(400).json({error: "Saldo Insuficiente!"})
     };
 
-    const statementOperation = {
-        amount,
+    const extratoOperation = {
+        quantidade,
         created_at: new Date(),
         type: "debit"
     }
 
-    customer.statement.push(statementOperation);
+    cliente.extrato.push(extratoOperation);
     return response.status(201).send();
 
 })
@@ -113,53 +114,52 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
 
 
 //buscar extrato por data
-app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
-    const { customer } = request;
+app.get("/extrato/date", verificaContaExistenteCPF, (request, response) => {
+    const { cliente } = request;
     const { date } = request.query;
     //formatando a data e informando ao filtro que o horário não é relevante.
     const dateFormat = new Date(date + " 00:00");
 
     //filtro para retornar o extrato somente da data informada.
-    const statement = customer.statement.filter((statement) =>
-    statement.created_at.toDateString() ===
+    const extrato = cliente.extrato.filter((extrato) =>
+    extrato.created_at.toDateString() ===
     new Date (dateFormat).toDateString()
     );
 
     //caso existe alguma movimentação neste dia, retorne o extrato
-    return response.json(statement)
+    return response.json(extrato)
 
 });
 
 //atualizar dados do cliente
-app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+app.put("/conta", verificaContaExistenteCPF, (request, response) => {
     const { name } = request.body;
-    const { customer } = request;
+    const { cliente } = request;
 
-    customer.name = name;
+    cliente.name = name;
     return response.status(201).send();
 });
 
 //obter dados da conta
-app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
-    const { customer } = request;
-    return response.json(customer);
+app.get("/conta", verificaContaExistenteCPF, (request, response) => {
+    const { cliente } = request;
+    return response.json(cliente);
 });
 
 //deletar conta
-app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
-    const { customer } = request;    
-    customers.splice(customer, 1);
-    return response.status(200).json(customers);
+app.delete("/conta", verificaContaExistenteCPF, (request, response) => {
+    const { cliente } = request;    
+    clientes.splice(cliente, 1);
+    return response.status(200).json(clientes);
 });
 
 //retornando o balanço da conta
-app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
-    const { customer } = request;
-    const balance = getBalance(customer.statement);
+app.get("/saldo", verificaContaExistenteCPF, (request, response) => {
+    const { cliente } = request;
+    const saldo = getSaldo(cliente.extrato);
     
-    return response.json(balance);
+    return response.json(saldo);
 });
-
 
 
 
